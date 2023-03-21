8d47f1d6a0b2a8573e176247fb92c1c3b9a2da36
anabfer <abf.ferreirac@gmail.com>
2023-03-21 15:47:37 -0300

    Exercícios Feitos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
 //Crie a const para a frase aqui
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+console.log(frase)
+
+//Crie uma nova string a partir da primeira, trocando verde por rosa, e azul por laranja;
+
+const novaFrase = frase.replaceAll("verde" , "rosa").replaceAll("azul" , "laranja")
+
+// Verifique se a nova string incui verde, se se inclui laranja.
+
+console.log(novaFrase)
+
+//tente fazer o trecho “mas não deixe o gato sair” ficar em letras maiúsculas, assim como o “BOAS VINDAS”
+ 
+const fraseCasa = "Jorge tem uma casa verde e com portão azul, com os dizeres: "
+const boasVindas= "BOAS VINDAS, mas não deixe o gato sair"
+const boasVindasMaiusculo = boasVindas.toUpperCase()
+
+console.log(fraseCasa + boasVindasMaiusculo)
+
